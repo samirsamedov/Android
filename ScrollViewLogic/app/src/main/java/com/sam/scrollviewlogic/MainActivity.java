4d1eacbdbc745f43e37b8cc0f81ec98b3ea7b12d
samirsamedow <l1113904@std.yildiz.edu.tr>
2016-07-13 15:33:43 +0300

    Scroll View Logic

--- ScrollViewLogic/app/src/main/java/com/sam/scrollviewlogic/MainActivity.java
+++ ScrollViewLogic/app/src/main/java/com/sam/scrollviewlogic/MainActivity.java
@@ -2,6 +2,10 @@ package com.sam.scrollviewlogic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         * ScrollView is main Frame
         * inside is we need a layout , I Choosed Linerlayout
         * and dont forget to close with what you opened
-        * <ScrollView   // open
+        * <ScrollView // // open
                 android:layout_width="match_parent"
                 android:layout_height="wrap_content">
 
@@ -25,9 +29,36 @@ public class MainActivity extends AppCompatActivity {
                     android:layout_width="match_parent"
                     android:layout_height="match_parent">
 
-                 </LinearLayout> // close
+                 </LinearLayout> //Close
 
             </ScrollView> // close
         * */
+
+        /*
+        ScrollView parentScroll = (ScrollView) findViewById(R.id.parentScroll);
+        ScrollView childScroll = (ScrollView) findViewById(R.id.childScroll);
+
+
+        parentScroll.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.v("PARENT", "PARENT TOUCH");
+                findViewById(R.id.childScroll).getParent()
+                        .requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+
+        childScroll.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.v("CHILD", "CHILD TOUCH");
+                // Disallow the touch request for parent scroll on touch of
+                // child view
+                v.getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
+        */
     }
 }
